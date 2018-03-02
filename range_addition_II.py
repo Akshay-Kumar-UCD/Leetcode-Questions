3d5a99445f035d7bd39000b588b4531110197934
Akshay Kumar <thekumar@ucdavis.edu>
2018-03-02 10:51:54 -0800

    interview prep

---
+++ range_addition_II.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxCount(self, m, n, ops):
+        """
+        :type m: int
+        :type n: int
+        :type ops: List[List[int]]
+        :rtype: int
+        """
+        if not ops:
+            return m*n
+        one, two = zip(*ops)
+        return min(one)*min(two)
